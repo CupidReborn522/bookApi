491af7cdaac04c3856e3a42d728f9e3520df8419
CupidReborn522 <reborn522@outlook.es>
2025-02-19 18:10:12 -0400

    sqlalchemy instalation

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
+from .routes.routes import routesBlueprint
 
 def create_app(settings):
+    
     app = Flask(__name__)
     # app config
     
     # Blueprints
-    app.register_blueprint()
+    
+    app.register_blueprint(routesBlueprint)
     
     with app.app_context():
         pass
---
+++ app/routes/routes.py
@@ -0,0 +1,3 @@
+from flask import Blueprint
+
+routesBlueprint = Blueprint('routes',__name__)
